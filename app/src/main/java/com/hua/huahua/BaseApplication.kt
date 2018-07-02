e2f6cfb068ab109d123a34dfcb8af43de1d616ef
zlh <1532767423@qq.com>
2018-07-02 16:38:52 +0800

    增加bug监控，使用bugly第三方观察

--- app/src/main/java/com/hua/huahua/BaseApplication.kt
+++ app/src/main/java/com/hua/huahua/BaseApplication.kt
@@ -1,10 +1,12 @@
 package com.hua.huahua
 
 import android.app.Application
+import com.tencent.bugly.crashreport.CrashReport
 
-class BaseApplication: Application() {
+class BaseApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        CrashReport.initCrashReport(applicationContext, "0e00cf4aa6", true)
     }
 }
